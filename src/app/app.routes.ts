d5ee3fa0ce2eba5140f2cfd1732975ce2e3a29ed
Wesley T. Benette <weluonf@gmail.com>
2024-09-18 18:40:43 -0300

    :sparkles: feat: Configures navigation routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './views/home-page/home-page.component';
+import { ErrorPageComponent } from './views/error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "", component: HomePageComponent},
+	{ path: '**', component: ErrorPageComponent }
+];
+
+@NgModule
+({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
