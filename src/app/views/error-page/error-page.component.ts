a1424bd45bf216742c7b0cb99c28cbe4149f50e6
Wesley T. Benette <weluonf@gmail.com>
2024-10-14 09:25:04 -0300

    :sparkles: feat: Implements Error Page.

--- src/app/views/error-page/error-page.component.ts
+++ src/app/views/error-page/error-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
 	selector: 'app-error-page',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 	templateUrl: './error-page.component.html',
 	styleUrl: './error-page.component.scss'
 })
-export class ErrorPageComponent {}
+export class ErrorPageComponent implements OnInit {
+
+	public code!: number
+
+	public constructor(
+		private _route: ActivatedRoute,
+		private _nav: Router
+	) {}
+
+	public ngOnInit(): void {
+		this.code = Number(this._route.snapshot.paramMap.get('code') ?? '404');
+	}
+
+	public getErrorMessage(code: number): string {
+		const errorMessages: { [key: number]: string } = {
+			404: 'Recurso não encontrado.',
+			500: 'Erro interno do servidor.'
+		};
+
+		return errorMessages[code] ?? errorMessages[404];
+	}
+
+	public gotoHome(): void {
+		this._nav.navigate(['/']);
+	}
+}
